227d70d6039aa56e6685297aefd1a8d10a97c5d5
dprat0821 <dprat0821@gmail.com>
2022-12-17 10:08:17 -0500

    added scripts

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--share --api --listen
+set ATTN_PRECISION=fp16
+set STABLE_DIFFUSION_COMMIT_HASH="c12d960d1ee4f9134c2516862ef991ec52d3f59e"
 
 call webui.bat
